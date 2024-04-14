a0d6c8f7baa7e9904445a9f115d129f82da463a5
tmorse01 <74744091+tmorse01@users.noreply.github.com>
2024-04-14 12:56:54 -0700

    themed chart

--- src/components/DonutChart.tsx
+++ src/components/DonutChart.tsx
@@ -1,250 +1,78 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-use-before-define */
-import React, { useState } from "react";
-import Pie, { ProvidedProps, PieArcDatum } from "@visx/shape/lib/shapes/Pie";
-import { scaleOrdinal } from "@visx/scale";
+import React from "react";
+import { Pie, Arc } from "@visx/shape";
 import { Group } from "@visx/group";
-import letterFrequency, {
-  LetterFrequency,
-} from "@visx/mock-data/lib/mocks/letterFrequency";
-import browserUsage, {
-  BrowserUsage as Browsers,
-} from "@visx/mock-data/lib/mocks/browserUsage";
-import { animated, useTransition, interpolate } from "@react-spring/web";
+import { scaleOrdinal } from "@visx/scale";
+import { tokens } from "@fluentui/react-theme";
 
-// data and types
-type BrowserNames = keyof Browsers;
-
-interface BrowserUsage {
-  label: BrowserNames;
-  usage: number;
-}
-
-const letters: LetterFrequency[] = letterFrequency.slice(0, 4);
-const browserNames = Object.keys(browserUsage[0]).filter(
-  (k) => k !== "date"
-) as BrowserNames[];
-const browsers: BrowserUsage[] = browserNames.map((name) => ({
-  label: name,
-  usage: Number(browserUsage[0][name]),
-}));
-
-// accessor functions
-const usage = (d: BrowserUsage) => d.usage;
-const frequency = (d: LetterFrequency) => d.frequency;
-
-// color scales
-const getBrowserColor = scaleOrdinal({
-  domain: browserNames,
-  range: [
-    "rgba(255,255,255,0.7)",
-    "rgba(255,255,255,0.6)",
-    "rgba(255,255,255,0.5)",
-    "rgba(255,255,255,0.4)",
-    "rgba(255,255,255,0.3)",
-    "rgba(255,255,255,0.2)",
-    "rgba(255,255,255,0.1)",
-  ],
-});
-const getLetterFrequencyColor = scaleOrdinal({
-  domain: letters.map((l) => l.letter),
-  range: [
-    "rgba(93,30,91,1)",
-    "rgba(93,30,91,0.8)",
-    "rgba(93,30,91,0.6)",
-    "rgba(93,30,91,0.4)",
-  ],
-});
-
-const defaultMargin = { top: 20, right: 20, bottom: 20, left: 20 };
-
-export type PieProps = {
+interface DonutChartProps {
   width: number;
   height: number;
-  margin?: typeof defaultMargin;
-  animate?: boolean;
-};
+}
+interface Expense {
+  category: string;
+  amount: number;
+}
 
-export default function DonutChart({
-  width,
-  height,
-  margin = defaultMargin,
-  animate = true,
-}: PieProps) {
-  const [selectedBrowser, setSelectedBrowser] = useState<string | null>(null);
-  const [selectedAlphabetLetter, setSelectedAlphabetLetter] = useState<
-    string | null
-  >(null);
+const expenses: Expense[] = [
+  { category: "Groceries", amount: 300 },
+  { category: "Utilities", amount: 200 },
+  { category: "Entertainment", amount: 150 },
+  { category: "Transportation", amount: 100 },
+  { category: "Healthcare", amount: 80 },
+];
 
-  if (width < 10) return null;
-
-  const innerWidth = width - margin.left - margin.right;
-  const innerHeight = height - margin.top - margin.bottom;
-  const radius = Math.min(innerWidth, innerHeight) / 2;
-  const centerY = innerHeight / 2;
-  const centerX = innerWidth / 2;
-  const donutThickness = 50;
-
+const colorScale = scaleOrdinal({
+  domain: expenses.map((e) => e.category),
+  range: [
+    tokens.colorPaletteGreenForeground1,
+    tokens.colorPaletteRedForeground1,
+    tokens.colorPaletteYellowForeground1,
+    tokens.colorPaletteBlueForeground2,
+    tokens.colorPaletteBerryForeground1,
+    tokens.colorPaletteLightGreenForeground1,
+    tokens.colorPaletteMarigoldForeground1,
+    tokens.colorPaletteDarkOrangeForeground1,
+    tokens.colorPalettePurpleForeground2,
+    tokens.colorPalettePinkForeground2,
+  ],
+});
+const DonutChart: React.FC<DonutChartProps> = ({ width, height }) => {
+  console.log(tokens);
+  const radius = Math.min(width, height) / 2;
+  const centerY = height / 2;
+  const centerX = width / 2;
+  const pieValue = (data: Expense) => data.amount;
   return (
     <svg width={width} height={height}>
-      <rect
-        rx={14}
-        width={width}
-        height={height}
-        fill="url('#visx-pie-gradient')"
-      />
-      <Group top={centerY + margin.top} left={centerX + margin.left}>
+      <Group top={centerY} left={centerX}>
         <Pie
-          data={
-            selectedBrowser
-              ? browsers.filter(({ label }) => label === selectedBrowser)
-              : browsers
-          }
-          pieValue={usage}
+          data={expenses}
+          pieValue={pieValue}
           outerRadius={radius}
-          innerRadius={radius - donutThickness}
+          innerRadius={radius * 0.6}
           cornerRadius={3}
           padAngle={0.005}
         >
-          {(pie) => (
-            <AnimatedPie<BrowserUsage>
-              {...pie}
-              animate={animate}
-              getKey={(arc) => arc.data.label}
-              onClickDatum={({ data: { label } }) =>
-                animate &&
-                setSelectedBrowser(
-                  selectedBrowser && selectedBrowser === label ? null : label
-                )
-              }
-              getColor={(arc) => getBrowserColor(arc.data.label)}
-            />
-          )}
-        </Pie>
-        <Pie
-          data={
-            selectedAlphabetLetter
-              ? letters.filter(
-                  ({ letter }) => letter === selectedAlphabetLetter
-                )
-              : letters
-          }
-          pieValue={frequency}
-          pieSortValues={() => -1}
-          outerRadius={radius - donutThickness * 1.3}
-        >
-          {(pie) => (
-            <AnimatedPie<LetterFrequency>
-              {...pie}
-              animate={animate}
-              getKey={({ data: { letter } }) => letter}
-              onClickDatum={({ data: { letter } }) =>
-                animate &&
-                setSelectedAlphabetLetter(
-                  selectedAlphabetLetter && selectedAlphabetLetter === letter
-                    ? null
-                    : letter
-                )
-              }
-              getColor={({ data: { letter } }) =>
-                getLetterFrequencyColor(letter)
-              }
-            />
-          )}
+          {(pie) => {
+            return pie.arcs.map((arc, i) => (
+              <g key={`arc-${arc.data.category}-${i}`}>
+                <Arc
+                  data={arc.data}
+                  innerRadius={radius * 0.6}
+                  outerRadius={radius}
+                  cornerRadius={3}
+                  startAngle={arc.startAngle}
+                  endAngle={arc.endAngle}
+                  padAngle={0.005}
+                  fill={colorScale(arc.data.category)}
+                />
+              </g>
+            ));
+          }}
         </Pie>
       </Group>
-      {animate && (
-        <text
-          textAnchor="end"
-          x={width - 16}
-          y={height - 16}
-          fill="white"
-          fontSize={11}
-          fontWeight={300}
-          pointerEvents="none"
-        >
-          Click segments to update
-        </text>
-      )}
     </svg>
   );
-}
-
-// react-spring transition definitions
-type AnimatedStyles = { startAngle: number; endAngle: number; opacity: number };
-
-const fromLeaveTransition = ({ endAngle }: PieArcDatum<any>) => ({
-  // enter from 360° if end angle is > 180°
-  startAngle: endAngle > Math.PI ? 2 * Math.PI : 0,
-  endAngle: endAngle > Math.PI ? 2 * Math.PI : 0,
-  opacity: 0,
-});
-const enterUpdateTransition = ({ startAngle, endAngle }: PieArcDatum<any>) => ({
-  startAngle,
-  endAngle,
-  opacity: 1,
-});
-
-type AnimatedPieProps<Datum> = ProvidedProps<Datum> & {
-  animate?: boolean;
-  getKey: (d: PieArcDatum<Datum>) => string;
-  getColor: (d: PieArcDatum<Datum>) => string;
-  onClickDatum: (d: PieArcDatum<Datum>) => void;
-  delay?: number;
 };
 
-function AnimatedPie<Datum>({
-  animate,
-  arcs,
-  path,
-  getKey,
-  getColor,
-  onClickDatum,
-}: AnimatedPieProps<Datum>) {
-  const transitions = useTransition<PieArcDatum<Datum>, AnimatedStyles>(arcs, {
-    from: animate ? fromLeaveTransition : enterUpdateTransition,
-    enter: enterUpdateTransition,
-    update: enterUpdateTransition,
-    leave: animate ? fromLeaveTransition : enterUpdateTransition,
-    keys: getKey,
-  });
-  return transitions((props, arc, { key }) => {
-    const [centroidX, centroidY] = path.centroid(arc);
-    const hasSpaceForLabel = arc.endAngle - arc.startAngle >= 0.1;
-
-    return (
-      <g key={key}>
-        <animated.path
-          // compute interpolated path d attribute from intermediate angle values
-          d={interpolate(
-            [props.startAngle, props.endAngle],
-            (startAngle, endAngle) =>
-              path({
-                ...arc,
-                startAngle,
-                endAngle,
-              })
-          )}
-          fill={getColor(arc)}
-          onClick={() => onClickDatum(arc)}
-          onTouchStart={() => onClickDatum(arc)}
-        />
-        {hasSpaceForLabel && (
-          <animated.g style={{ opacity: props.opacity }}>
-            <text
-              fill="white"
-              x={centroidX}
-              y={centroidY}
-              dy=".33em"
-              fontSize={9}
-              textAnchor="middle"
-              pointerEvents="none"
-            >
-              {getKey(arc)}
-            </text>
-          </animated.g>
-        )}
-      </g>
-    );
-  });
-}
+export default DonutChart;
