5beb31406205f4e001edadcf931603a07afab041
tmorse01 <74744091+tmorse01@users.noreply.github.com>
2024-11-19 21:23:57 -0800

    A test file

---
+++ frontend/src/File.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const File: React.FC = () => {
+  return (
+    <div>
+      <h1>Test File</h1>
+      <p>This is a test file.</p>
+    </div>
+  );
+};
+
+export default File;
